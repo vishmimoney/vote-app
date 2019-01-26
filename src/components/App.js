c8dca65a46d8dc6c39dd7ce2231aec470cca16a2
vishmimoney <vishmimoney@gmail.com>
2019-01-26 11:30:04 +0100

    Add initial project files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        my app
+      </div>
+    );
+  }
+}
+
+export default App;
